bbb0a41d5b94f88981473716a483786af7861e5a
Esther Mejia <95319280+EstherMejia20@users.noreply.github.com>
2022-06-13 18:15:01 -0600

    Update get-quote.py
    quotes.txt

--- get-quote.py
+++ get-quote.py
@@ -1,5 +1,10 @@
-def main(primary):
-  print("Keep it logically awesome.")
+#def main(primary):
+ # print("Keep it logically awesome.")
+    f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  print(quotes[0])
 
   #f = open("quotes.txt")
   #quotes = f.readlines()
@@ -7,5 +12,5 @@ def main(primary):
 
   #print(quotes)
 
-if __name__== "__main__":
-  main(primary)
+#if __name__== "__main__":
+ # main(primary)
